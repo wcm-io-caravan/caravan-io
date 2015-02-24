79547cff0ff23ce7bf20222328d01c771304c943
Christoph Nagel <cnagel@pro-vision.de>
2015-02-24 15:27:17 +0100

    Adds Hystrix Codahale Metrics output

---
+++ http/src/main/java/io/wcm/caravan/io/http/impl/HystrixMetricsMounter.java
@@ -0,0 +1,51 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.caravan.io.http.impl;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Reference;
+
+import com.codahale.metrics.MetricRegistry;
+import com.netflix.hystrix.contrib.codahalemetricspublisher.HystrixCodaHaleMetricsPublisher;
+import com.netflix.hystrix.strategy.HystrixPlugins;
+
+/**
+ * Mounter for the Hystrix Metrics plugin.
+ */
+@Component
+public class HystrixMetricsMounter {
+
+  @Reference
+  private MetricRegistry metricRegistry;
+
+  @Activate
+  protected void activate() {
+    HystrixCodaHaleMetricsPublisher publisher = new HystrixCodaHaleMetricsPublisher(metricRegistry);
+    HystrixPlugins.getInstance().registerMetricsPublisher(publisher);
+  }
+
+  @Deactivate
+  protected void deactivate() {
+    HystrixPlugins.reset();
+  }
+
+}
