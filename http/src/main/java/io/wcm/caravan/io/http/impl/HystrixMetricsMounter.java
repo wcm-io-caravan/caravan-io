e892cf1a540649caefee5b892c17d8be90fd03a4
guiquan weng <gweng@pro-version.de>
2016-08-30 12:57:03 +0200

    revert the integration of graphite server.  all changes should be moved to commons/metrics bundle

--- http/src/main/java/io/wcm/caravan/io/http/impl/HystrixMetricsMounter.java
+++ http/src/main/java/io/wcm/caravan/io/http/impl/HystrixMetricsMounter.java
@@ -19,20 +19,12 @@
  */
 package io.wcm.caravan.io.http.impl;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.graphite.Graphite;
-import com.codahale.metrics.graphite.GraphiteReporter;
 import com.netflix.hystrix.contrib.codahalemetricspublisher.HystrixCodaHaleMetricsPublisher;
 import com.netflix.hystrix.strategy.HystrixPlugins;
 
@@ -42,106 +34,19 @@ import com.netflix.hystrix.strategy.HystrixPlugins;
 @Component
 public class HystrixMetricsMounter {
 
-  private static final Logger LOG = LoggerFactory.getLogger(HystrixMetricsMounter.class);
-
   @Reference
   private MetricRegistry metricRegistry;
-  @Reference
-  private GraphiteIntegrationConfig graphiteIntegrationConfig;
-
-  private GraphiteRegistration graphiteRegistration = new GraphiteRegistration();
 
   @Activate
-  void activate() throws IOException {
+  protected void activate() {
     HystrixCodaHaleMetricsPublisher publisher = new HystrixCodaHaleMetricsPublisher(metricRegistry);
     HystrixPlugins.getInstance().registerMetricsPublisher(publisher);
-
-    // stop the connections
-    graphiteRegistration.interrupt();
-    graphiteRegistration.start();
   }
 
   @Deactivate
-  protected void deactivate() throws IOException {
-    graphiteRegistration.interrupt();
-
+  protected void deactivate() {
     metricRegistry.getNames().stream().forEach(name -> metricRegistry.remove(name));
     HystrixPlugins.reset();
-
-  }
-
-  /**
-   * register the graphite reporter only if the first connection could be established, to prevent lots of connection exception in logs.
-   */
-  private class GraphiteRegistration extends Thread {
-
-    private Graphite graphiteServer;
-    private static final int RETRY_TIME_IN_MINUTES = 2;
-
-    @Override
-    public void run() {
-      closeConnection();
-
-      if (graphiteIntegrationConfig.isEnabled()) {
-        String hostName = graphiteIntegrationConfig.getGraphiteHostName();
-        int port = graphiteIntegrationConfig.getGraphiteSocketPort();
-
-        LOG.info("Graphite publisher is enabled.");
-        graphiteServer = new Graphite(new InetSocketAddress(hostName, port));
-
-        if (establishConnection()) {
-          int pushInterval = graphiteIntegrationConfig.getPushInterval();
-          GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).build(graphiteServer);
-          reporter.start(pushInterval, TimeUnit.SECONDS);
-          LOG.info("Sending every {} seconds to {}:{}", pushInterval, hostName, port);
-        }
-      }
-      else {
-        LOG.info("Graphite publisher is disabled.");
-      }
-    }
-
-    /**
-     * establish a connection to the graphite. if is failed, wait {@value #RETRY_TIME_IN_MINUTES} minutes and try again
-     * until the connection is established successfully or the thread is stopped.
-     *
-     * @return
-     */
-    private boolean establishConnection() {
-      while (!Thread.currentThread().isInterrupted()) {
-        try {
-          graphiteServer.connect();
-          LOG.info("Connection to graphite server is established.");
-          return true;
-        }
-        catch (IOException e) {
-          LOG.error("Unable to connect to {}:{}! wait {} minutes and reconnect. ERROR: {}",
-            graphiteIntegrationConfig.getGraphiteHostName(),
-            graphiteIntegrationConfig.getGraphiteSocketPort(),
-            RETRY_TIME_IN_MINUTES,
-            e.getMessage());
-          try {
-            Thread.sleep(TimeUnit.MINUTES.toMillis(RETRY_TIME_IN_MINUTES));
-          }
-          catch (InterruptedException e1) {
-            return false;
-          }
-        }
-      }
-      return false;
-    }
-
-
-    void closeConnection() {
-      if (graphiteServer != null && graphiteServer.isConnected()) {
-        try {
-          graphiteServer.close();
-        }
-        catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-      }
-    }
   }
 
 }
